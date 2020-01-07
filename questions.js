e3c44d3275f0b908e2e375261a49599e302323f9
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-01-06 20:44:54 -0600

    adding files

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+
+      title: "In the above syntax, the datatype within the square brackets must be:",
+      choices: ["An integer", "A String" , "An object", "None of the mentioned"],
+      answer: "A String"
+    },
+    {
+       title:  "What kind of Javascript is also called client-side JavaScript:",
+       choices: ["Microsoft" ,"Navigator" ,"LiveWire" ,"Native"],
+       answer: "Navigator"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<js>" ,"<scripting>" ,"<script>" ,"<javascript>" ],
+
+    },
+  ];
---
+++ script.js
@@ -0,0 +1,79 @@
+
+// var questions = [
+
+
+// for(var i=0; i < questions.length; i++) {
+// var response = window.prompt(questions[i].prompt)
+// if(response == questions[i].answer){
+//     score++;
+//     alert("Correct!");
+// }   else{
+//     alert("Wrong!");
+//     }
+
+// alert("answer" + score + "/" + questions.length);
+// ]
+console.log("questions" + questions)
+for(var i=0; i< questions.length; i++) {
+    var questiontag = document.createElement("H1");
+    var question = document.createTextNode(questions[i].title);
+    questiontag.appendChild(question);
+    document.body.appendChild(questiontag);
+for(var i=1; i< questions.length; 1++) {
+    var questiontag = document.createElement("H1");
+    var question = document.createTextNode(questions[+i].title);
+    questiontag.appendChild(question);
+    document.body.appendChild(questiontag);
+for(var i=+1; i< questions.length; 1++) {
+    var questiontag = document.createElement("H1");
+    var question = document.createTextNode(questions[+i].title);
+    questiontag.appendChild(question);
+    document.body.appendChild(questiontag);
+for(var i=+1; i< questions.length; 1++) {
+    var questiontag = document.createElement("H1");
+    var question = document.createTextNode(questions[+i].title);
+    questiontag.appendChild(question);
+    document.body.appendChild(questiontag);
+for(var i=+1; i< questions.length; 1++){
+    var questiontag = document.createElement("H1");
+    var question = document.createTextNode(questions[+i].title);
+    questiontag.appendChild(question);
+    document.body.appendChild(questiontag);
+
+}
+
+}
+
+}
+
+}
+}
+// for loop to Answer choices:
+console.log("answers" + answers)
+for(var i=0; 
+
+// create button tag for each answer choice:
+<button> onclick="myFunction()">["strings", "booleans", "alerts", "numbers"],</button>
+<button> onclick="myFunction()">["quotes", "curly brackets", "parentheses", "square brackets"],</button>
+<button> onclick="myFunction()">["An integer", "A String" , "An object", "None of the mentioned"],</button>
+<button> onclick="myFunction()">["Microsoft" ,"Navigator" ,"LiveWire" ,"Native"],</button>
+<button> onclick="myFunction()">["js" ,"scripting" ,"script" ,"javascript"],</button>
+
+
+
+
+
+
+
+// create event listener for button:
+var document = document
+
+document.getElementById("Go Back").addEventListener("click", function(){
+    
+    document.getElementById("demo").innerHTML = "Go Back"
+document.getElementById("Clear Highscore").addEventListener("click", function(){
+    
+document.getElementById("demo").innerHTML = "Clear Highscore" 
+});
+ 
+}
